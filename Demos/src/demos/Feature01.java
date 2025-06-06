ce03f1e15a389c3119befdb4065d0fe2bc6df564
prave <prave@DESKTOP-UI45AN4>
2025-06-06 22:05:59 +0530

    abcd

--- Demos/src/demos/Feature01.java
+++ Demos/src/demos/Feature01.java
@@ -5,6 +5,8 @@ public class Feature01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("Feature01");
+          System.out.println("Feature02");
+         
 	}
 
 }
